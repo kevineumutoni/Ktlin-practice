cea2bd5556ab46fd739fdd70f0b17efc3893035c
kezaqueen <rutagengwakezaqueengmail.com>
2025-02-26 10:54:15 +0300

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main () {
+    val name = "keza Queen"
+    println(name)
+    var age:String="born in 2004"
+    println(age)
+    var country: String = "lives in Rwanda"
+    println(country)
+    val height: Float= 1.7f
+    println(height)
+    val weight:Double = 60.00
+    println(weight)
+    val isKezaAmazing: Boolean= true
+    val iskezaselfish: Boolean= false
+    println(isKezaAmazing)
+    println(iskezaselfish)
+
+
+}
